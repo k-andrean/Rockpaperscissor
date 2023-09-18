dafc8fbf973cbdc3979ed3bf40a5dff4d019dc42
kelvin <baganceshop@gmail.com>
2023-09-18 18:59:28 +0700

    refactor rock paper scissor game adding html to display game result

--- game.js
+++ game.js
@@ -1,110 +1,140 @@
-let round = 1;
-const count = 5;
-let computerSelection; 
+const rockSelect = 'Rock!';
+const paperSelect = 'Paper!';
+const scissorSelect = 'Scissor!';
+const zeroValue = 0;
+const emptyString = '';
+
+
+let playerScore = zeroValue;
+let computerScore = zeroValue; 
+let draw = zeroValue;
+
+
 let playerSelection;
-let playerScore = 0;
-let computerScore = 0; 
-let draw = 0;
+let computerSelection;
 
 
-// function prompting and returning player choice
 
-function getPlayerChoice(){
-    let playerChoice =  prompt("please enter your choice rock, paper or scissor: ").toLowerCase();
+function getPlayerChoice(clickedButton){
+    const value = clickedButton.value;
 
-    if (playerChoice  == 'rock'){
-        return "Rock!";
-    }
-    else if (playerChoice  == 'scissor'){
-        return "Scissor!";
-    }
-    else if (playerChoice  == 'paper'){
-        return "Paper!";
-    }
-    else {
-        return "Please enter appropriate choice";
-    }
+    if (value == 'rock') return rockSelect;
+    else if (value == 'scissor') return scissorSelect;
+    return paperSelect;
+
 };
-
-
-// function returning computer choice based on math random integer value
 
 function getComputerChoice(){
     let computerChoice =  Math.floor((Math.random() * 3) + 1);
 
-    if (computerChoice  == 1){
-        return "Rock!";
-    }
-    else if (computerChoice  == 2){
-        return "Scissor!";
-    }
-    else {
-        return "Paper!";
-    }
+    if (computerChoice  == 1) return rockSelect;
+    else if (computerChoice  == 2) return scissorSelect;
+    return paperSelect;
+
 };
 
-// function simulating game round and returning result while incrementing the score
-
-function gameRound(){
-
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
+// update game score and display in html
+function updateGameScore(playerSelection, computerSelection){
 
 
+    roundSelection.textContent = `Player choose: ${playerSelection}, Computer choose: ${computerSelection}`;
 
-    if ((playerSelection == 'Rock!' && computerSelection == 'Rock!')||(playerSelection == 'Paper!' && computerSelection == 'Paper!')||(playerSelection == 'Scissor!' && computerSelection == 'Scissor!')){
+
+    if (playerSelection == computerSelection){
         draw++;
-        return "it's a draw!";
+        roundWinner.textContent = "it's a draw!";
+
     }
     else if ((playerSelection == 'Rock!' && computerSelection == 'Scissor!')||(playerSelection == 'Scissor!' && computerSelection == 'Paper!')||(playerSelection == 'Paper!' && computerSelection == 'Rock!')){
         playerScore++;
-        return "Player win!";
+        roundWinner.textContent = "Player win!";
     }
-    else if ((playerSelection == 'Rock!' && computerSelection == 'Paper!')||(playerSelection == 'Scissor!' && computerSelection == 'Rock!')||(playerSelection == 'Paper!' && computerSelection == 'Scissor!')){
+    else { 
         computerScore++;
-        return "Computer win!";
+        roundWinner.textContent = "Computer win!";
     }
-    else {
-        return "please reenter appropriate choice";
+
+    total.textContent = `Player win: ${playerScore}, Computer win: ${computerScore}, Draw: ${draw}`;
+
+};
+
+
+function gameRound(clickedButton){
+
+    playerSelection = getPlayerChoice(clickedButton);
+    computerSelection = getComputerChoice();
+    
+    updateGameScore(playerSelection, computerSelection);
+
+};
+
+
+
+function playRound(clickedButton){
+
+    gameRound(clickedButton);
+
+    if (playerScore >= 5 || computerScore >= 5){
+
+        const result =
+        (playerScore > computerScore) ? 
+        'Player is the winner'
+        : (computerScore > playerScore) ? 
+        'Computer is the winner'
+        : 'The game is Draw';
+
+        gameWinner.textContent = result;
+
+        gameWinner.classList.remove('hidden');
+        play.classList.remove('hidden');
     }
     
-};
-
-// function for logging score and determining final winner of the game
-
-function getGameScore(){
-
-    console.log("Player win: ", playerScore);
-    console.log("Computer win: ", computerScore);
-    console.log("Draw: ", draw);
-
-    if (playerScore > computerScore){
-        console.log("Player win paper rock scissor game")
-    }
-    else if (playerScore < computerScore){
-        console.log("Computer win paper rock scissor game")
-    }
-    else {
-        console.log("The game conclusion is draw, nice game!")
-    };
-
-    console.log("Game ended!");
-};
+            
+};    
 
 
-// main function simulating five rounds of rock paper scissor game
+// reset game score to beginning
+function resetGame(){
 
-function playRound(){
-    for(let i = count; i > 0; i--){
-        console.log("Round: " + round);
-        console.log(gameRound());
-        round++;
-    }
+    total.textContent = emptyString;
+    roundWinner.textContent = emptyString;
+    gameWinner.textContent = emptyString;
+    roundSelection.textContent = emptyString;
 
-    getGameScore();
+    playerScore = zeroValue;
+    computerScore = zeroValue;
+    draw = zeroValue;
+    console.log(playerScore);
 
-};
+    gameWinner.classList.add('hidden');
+    play.classList.add('hidden');
+}
 
 
 
-playRound();
+// commonly used variable across game function
+const playerChoice = document.querySelectorAll('.player-choice');
+const play = document.getElementById('play');
+const total = document.querySelector('.total');
+const roundWinner = document.querySelector('.round-winner');
+const gameWinner = document.getElementById('game-winner');
+const roundSelection = document.querySelector('.round-selection');
+
+
+
+
+playerChoice.forEach((choice) => choice.addEventListener('click', (e) => playRound(e.target)));
+
+play.addEventListener('click', () =>{
+    resetGame()
+});
+
+
+
+
+
+
+
+
+
+
